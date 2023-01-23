61f915437449daed367e9ab5096658b78184a45b
Reilly Lawson <rbl2991@rit.edu>
2023-01-23 11:47:25 -0500

    Working on methods of the roster class

--- PEs/PE-Lists of Objects/Roster.cs
+++ PEs/PE-Lists of Objects/Roster.cs
@@ -12,19 +12,66 @@ namespace PE_Lists_of_Objects
         // Fields
         //-----------------------------
 
+        private string name;
+        private List<Student> students = new List <Student>();
 
         //-----------------------------
         // Properties 
         //-----------------------------
 
+        /// <summary>
+        /// Get only property that gives the roster's name
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+        }
+
+        /// <summary>
+        /// Get only property that gives the list of students
+        /// </summary>
+        public List<Student> Students
+        {
+            get { return students; }
+        }
         
         //-----------------------------
         // Constructors
         //-----------------------------
 
+        /// <summary>
+        /// Parameterized constructor that takes the roster's name and list of students
+        /// </summary>
+        /// <param name="name"> takes in a string </param>
+        /// <param name="students"> takes in a list of objects of the student class </param>
+        public Roster(string name, List<Student> students)
+        {
+            this.name = name;
+            this.students = students;
+        }
 
         //-----------------------------
         // Methods
         //-----------------------------
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Student SearchByName(string name)
+        {
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (name == students[i].Name)
+                        
+                {
+                    return students[i];
+                }
+            }
+
+            return null;
+
+        }
     }
 }
