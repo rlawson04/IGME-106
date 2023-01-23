b4d78e5d55825dd677355b39a3c91a4e433863cc
Reilly Lawson <rbl2991@rit.edu>
2023-01-22 22:13:23 -0500

    Final Version

---
+++ PEs/RBL_OORefactoring/Player.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RBL_OORefactoring
+{
+    /// <summary>
+    /// Base class that will take in a string and int
+    /// to store information about a player
+    /// </summary>
+    internal class Player
+    {
+        //-------------------------------------
+        // Fields
+        //-------------------------------------
+
+        private int score;
+        private string name;
+
+
+        //-------------------------------------
+        // Properties
+        //-------------------------------------
+
+        /// <summary>
+        /// Get only property to return the score of the player
+        /// </summary>
+        public int Score
+        {
+            get { return score; }
+        }
+
+        /// <summary>
+        /// Get only property to return the name of the player
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+        }
+
+
+        //-------------------------------------
+        // Constructors
+        //-------------------------------------
+
+        /// <summary>
+        /// Parameterized constructor that takes name and score
+        /// </summary>
+        /// <param name="name"> takes in a string </param>
+        /// <param name="score"> takes in an int </param>
+        public Player(string name, int score)
+        {
+            this.name = name;
+            this.score = score;
+        }
+
+        //-------------------------------------
+        // Methods
+        //-------------------------------------
+    }
+}
--- PEs/RBL_OORefactoring/Program.cs
+++ PEs/RBL_OORefactoring/Program.cs
@@ -1,129 +1,69 @@
-﻿namespace RBL_OORefactoring
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace RBL_OORefactoring
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Keep track of a name and score of a bunch of players
-            string p1Name = "tbd";
-            string p2Name = "tbd";
-            string p3Name = "tbd";
-            string p4Name = "tbd";
-            string p5Name = "tbd";
-            int p1Score = 0;
-            int p2Score = 0;
-            int p3Score = 0;
-            int p4Score = 0;
-            int p5Score = 0;
+            // Create instance of random variable to use throughout
+            Random rng = new Random();
+            
             // Find out how many players the user actually wants
-            Console.Write("Enter number of players (up to 5): ");
-            string numPlayersString = Console.ReadLine();
-            int numPlayers = int.Parse(numPlayersString);
-            // Get the names
+            Console.Write("Enter number of players (up to 10): ");
+            
+            // TryParse until acceptable integer answer between 1-10
+            int number;
+            bool sucess = int.TryParse(Console.ReadLine(), out number);
+            while (!sucess || number < 1 || number > 10)
+            {
+                Console.Write("Enter number of players (up to 10): ");
+                sucess = int.TryParse(Console.ReadLine(), out number);
+            }
             Console.WriteLine();
+
+            // Variable to keep track of the number of players for iteration through array
+            int numPlayers = number;
+
+            // Create array to add player objects to
+            Player[] players = new Player[numPlayers];
+
+            // Get the names of all the players
             for (int i = 0; i < numPlayers; i++)
             {
-                Console.Write("Enter player {0}'s name: ", i);
+                Console.Write($"Enter player {i+1}'s name: ");
                 string name = Console.ReadLine().Trim().ToUpper();
-                switch (i)
-                {
-                    case 0:
-                        p1Name = name;
-                        break;
-                    case 1:
-                        p2Name = name;
-                        break;
-                    case 2:
-                        p3Name = name;
-                        break;
-                    case 3:
-                        p4Name = name;
-                        break;
-                    case 4:
-                        p5Name = name;
-                        break;
-                    default:
-                        Console.WriteLine("Uh oh...");
-                        break;
-                }
+
+                // Take the given name and create player instance
+                Player player = new Player(name, rng.Next(101));
+
+                // Add player to array
+                players[i] = player;
             }
-            // Randomly set the scores of the players
+            Console.WriteLine();
+            
+            // Prints each player with their score
             for (int i = 0; i < numPlayers; i++)
             {
-                Random rng = new Random();
-            switch (i)
-                {
-                    case 0:
-                        p1Score = rng.Next(101);
-                        break;
-                    case 1:
-                        p2Score = rng.Next(101);
-                        break;
-                    case 2:
-                        p3Score = rng.Next(101);
-                        break;
-                    case 3:
-                        p4Score = rng.Next(101);
-                        break;
-                    case 4:
-                        p5Score = rng.Next(101);
-                        break;
-                    default:
-                        Console.WriteLine("Uh oh...");
-                        break;
-                }
+                Console.WriteLine($"Player {players[i].Name}," +
+                    $" has a score of {players[i].Score}");
             }
-            // Print the player info
             Console.WriteLine();
-            Console.WriteLine("Player 1, {0}, has a score of {1}",
-            p1Name, p1Score);
-            Console.WriteLine("Player 2, {0}, has a score of {1}",
-            p2Name, p2Score);
-            Console.WriteLine("Player 3, {0}, has a score of {1}",
-            p3Name, p3Score);
-            Console.WriteLine("Player 4, {0}, has a score of {1}",
-            p4Name, p4Score);
-            Console.WriteLine("Player 5, {0}, has a score of {1}",
-            p5Name, p5Score);
-            // Print the winner
-            string winnerName = "no one";
-            if (numPlayers > 0)
+
+            // Variable to find out the winner
+            string hiScore = players[0].Name;
+
+            // Checks players score against the one below it
+            for (int i = 0; i < numPlayers -1; i++)
             {
-                winnerName = p1Name;
-            }
-            if (numPlayers > 1)
-            {
-                if (p2Score > p1Score)
+                if(players[i+1].Score > players[i].Score)
                 {
-                    winnerName = p2Name;
+                    hiScore = players[i+1].Name;
                 }
             }
-            if (numPlayers > 2)
-            {
-                if (p3Score > p1Score && p3Score > p2Score)
-            {
-                    winnerName = p3Name;
-                }
-            }
-            if (numPlayers > 3)
-            {
-                if (p4Score > p1Score && p4Score > p2Score && p4Score > p3Score)
-                {
-                    winnerName = p4Name;
-                }
-            }
-            if (numPlayers > 4)
-            {
-                if (p5Score > p1Score && p5Score > p2Score && p5Score > p3Score && p5Score > p4Score)
-                {
-                    winnerName = p5Name;
-                }
-            }
-            if (numPlayers > 5)
-            {
-                Console.WriteLine("Uh oh...");
-            }
-            Console.WriteLine("The winner is {0}!", winnerName);
+
+            // Print winner
+            Console.WriteLine("The winner is {0}!", hiScore);
         }
     }
 }
