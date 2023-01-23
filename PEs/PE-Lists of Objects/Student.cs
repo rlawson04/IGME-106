852955cc259fc5325819c1b3330e882253ce921c
Reilly Lawson <rbl2991@rit.edu>
2023-01-23 11:19:35 -0500

    Working on Student class

--- PEs/PE-Lists of Objects/Student.cs
+++ PEs/PE-Lists of Objects/Student.cs
@@ -12,13 +12,48 @@ namespace PE_Lists_of_Objects
         // Fields
         //-----------------------------
 
+        private string name;
+        private string major;
+        private int year;
+
         //-----------------------------
         // Properties 
         //-----------------------------
 
+        /// <summary>
+        /// Get only property that gives the student's name
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+        }
+
+        /// <summary>
+        /// Get only property that gives the student's major
+        /// </summary>
+        public string Major
+        {
+            get { return major; }
+        }
+
+        /// <summary>
+        /// Get only property that gives the student's year
+        /// </summary>
+        public int Year
+        {
+            get { return year; }
+        }
+
         //-----------------------------
         // Constructors
         //-----------------------------
+
+        public Student(string name, string major, int year)
+        {
+            this.name = name;
+            this.major = major;
+            this.year = year;
+        }
 
         //-----------------------------
         // Methods
