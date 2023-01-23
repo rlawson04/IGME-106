ad35c1db6b5730da961352acfc49016f95982dc4
Reilly Lawson <rbl2991@rit.edu>
2023-01-23 11:17:19 -0500

    Initial Commit

---
+++ PEs/PE-Lists of Objects/Roster.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_Lists_of_Objects
+{
+    internal class Roster
+    {
+        //-----------------------------
+        // Fields
+        //-----------------------------
+
+        private string name;
+        private string major;
+        private int year;
+
+        //-----------------------------
+        // Properties 
+        //-----------------------------
+
+        /// <summary>
+        /// Get only property that gives the student's name
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+        }
+
+        /// <summary>
+        /// Get only property that gives the student's major
+        /// </summary>
+        public string Major 
+        { 
+            get { return major; } 
+        }
+
+        /// <summary>
+        /// Get only property that gives the student's year
+        /// </summary>
+        public int Year 
+        { 
+            get { return year; }
+        }
+
+        //-----------------------------
+        // Constructors
+        //-----------------------------
+
+        public Student(string name, string major, int year)
+        {
+            this.name = name;
+            this.major = major;
+            this.year = year;
+        }
+
+        //-----------------------------
+        // Methods
+        //-----------------------------
+    }
+}
---
+++ PEs/PE-Lists of Objects/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_Lists_of_Objects
+{
+    internal class Student
+    {
+        //-----------------------------
+        // Fields
+        //-----------------------------
+
+        //-----------------------------
+        // Properties 
+        //-----------------------------
+
+        //-----------------------------
+        // Constructors
+        //-----------------------------
+
+        //-----------------------------
+        // Methods
+        //-----------------------------
+    }
+}
