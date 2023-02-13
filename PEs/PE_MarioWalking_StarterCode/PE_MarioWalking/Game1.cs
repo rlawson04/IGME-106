957795a72248b68eb8783019f029ca47bd313242
rlawson04 <rbl2991@rit.edu>
2023-02-13 11:13:52 -0500

    Initial Commit

---
+++ PEs/PE_MarioWalking_StarterCode/PE_MarioWalking/Game1.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace PE_MarioWalking
+{
+
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        // The Mario to draw depending on the current state
+        private Mario mario;
+
+        // Constructor
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Sets up the mario location
+            Vector2 marioLoc = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+
+            // Load the single spritesheet and create a new Mario object
+            Texture2D spriteSheet = Content.Load<Texture2D>("Mario");
+
+            mario = new Mario(spriteSheet, marioLoc, MarioState.FaceRight);
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // Handles animation for you
+            mario.UpdateAnimation(gameTime);
+
+            // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+            // PRACTICE EXERCISE: Add your finite state machine code (switch statement) here!
+            // - Be sure to check the finite state machine's state first
+            // - Then check for specific transitions inside each state (may require keyboard input)
+            // - Update Mario's state as needed
+
+            // Step 1: Grab user input
+
+            // Step 2: Change state
+
+            // Step 3: Move Mario only when walking
+
+            // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+            base.Update(gameTime);
+        }
+
+
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // Begin the sprite batch
+            _spriteBatch.Begin();
+
+            mario.Draw(_spriteBatch);
+
+            // End the sprite batch
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+    }
+}
---
+++ PEs/PE_MarioWalking_StarterCode/PE_MarioWalking/Mario.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace PE_MarioWalking
+{
+    // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    // PRACTICE EXERCISE:  Enums are typically declared here!
+
+    // Mario State enum
+    enum MarioState
+    {
+        FaceLeft,
+        WalkLeft,
+        FaceRight,
+        WalkRight // Add state(s) to support crouching
+    }
+
+    // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+    class Mario
+    {
+        Vector2 marioLoc;       // Mario's location on the screen
+        MarioState state;
+
+        // Texture and drawing
+        Texture2D spriteSheet;  // The single image with all of the animation frames
+
+        // Animation
+        int frame;              // The current animation frame
+        double timeCounter;     // The amount of time that has passed
+        double fps;             // The speed of the animation
+        double timePerFrame;    // The amount of time (in fractional seconds) per frame
+
+        // Constants for "source" rectangle (inside the image)
+        const int WalkFrameCount = 3;       // The number of frames in the animation
+        const int MarioRectOffsetY = 116;   // How far down in the image are the frames?
+        const int MarioRectHeight = 72;     // The height of a single frame
+        const int MarioRectWidth = 44;      // The width of a single frame
+
+        // Properties to get & set Mario's current location and animation state
+        public float X
+        {
+            get
+            {
+                return this.marioLoc.X;
+            }
+            set
+            {
+                this.marioLoc.X = value;
+            }
+        }
+
+        public MarioState State {
+            get { return state; }
+            set { state = value; }
+        }
+
+        // The constructor takes the sprite sheet to use and the initial location
+        public Mario(Texture2D spriteSheet, Vector2 marioLoc, MarioState startingState)
+        {
+            this.spriteSheet = spriteSheet;
+            this.marioLoc = marioLoc;
+            this.state = startingState;
+
+            // Initialize
+            fps = 10.0;                     // Will cycle through 10 walk frames per second
+            timePerFrame = 1.0 / fps;       // Time per frame = amount of time in a single walk image
+        }
+
+        /// <summary>
+        /// Updates mario's animation as necessary
+        /// </summary>
+        /// <param name="gameTime">Time information</param>
+        public void UpdateAnimation(GameTime gameTime)
+        {
+            // Handle animation timing
+            // - Add to the time counter
+            // - Check if we have enough "time" to advance the frame
+
+            // How much time has passed?  
+            timeCounter += gameTime.ElapsedGameTime.TotalSeconds;
+
+            // If enough time has passed:
+            if (timeCounter >= timePerFrame)
+            {
+                frame += 1;                     // Adjust the frame to the next image
+
+                if (frame > WalkFrameCount)     // Check the bounds - have we reached the end of walk cycle?
+                    frame = 1;                  // Back to 1 (since 0 is the "standing" frame)
+
+                timeCounter -= timePerFrame;    // Remove the time we "used" - don't reset to 0
+                                                // This keeps the time passed 
+            }
+        }
+
+        /// <summary>
+        /// Draws Mario onto the given sprite batch based on his current state (and
+        /// the current frame in the animation)
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+            // PRACTICE EXERCISE: Check the finite state machine state here to
+            // determine how exactly to draw Mario
+            DrawWalking(SpriteEffects.None, spriteBatch); // You may alter/remove this line
+
+            // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+        }
+
+        /// <summary>
+        /// Draws only the left-most frame of mario, which is him standing
+        /// </summary>
+        /// <param name="flipSprite">
+        /// Enum values for flipping the sprite horizontally
+        /// and or vertically
+        /// </param>
+        private void DrawStanding(SpriteEffects flipSprite, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                spriteSheet,                    // - The texture to draw
+                marioLoc,                       // - The location to draw on the screen
+                new Rectangle(                  // - The "source" rectangle
+                    0,                          //   - This rectangle specifies
+                    MarioRectOffsetY,           //	   where "inside" the texture
+                    MarioRectWidth,             //     to get pixels (We don't want to
+                    MarioRectHeight),           //     draw the whole thing)
+                Color.White,                    // - The color
+                0,                              // - Rotation (none currently)
+                Vector2.Zero,                   // - Origin inside the image (top left)
+                1.0f,                           // - Scale (100% - no change)
+                flipSprite,                     // - Can be used to flip the image
+                0);                             // - Layer depth (unused)
+        }
+
+        /// <summary>
+        /// Draws mario running, based on the current FRAME field
+        /// which is changed by the code in Update
+        /// </summary>
+        /// <param name="flipSprite">
+        /// Enum values for flipping the sprite horizontally
+        /// and or vertically
+        /// </param>
+        private void DrawWalking(SpriteEffects flipSprite, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                spriteSheet,                    // - The texture to draw
+                marioLoc,                       // - The location to draw on the screen
+                new Rectangle(                  // - The "source" rectangle
+                    frame * MarioRectWidth,     //   - This rectangle specifies
+                    MarioRectOffsetY,           //	   where "inside" the texture
+                    MarioRectWidth,             //     to get pixels (We don't want to
+                    MarioRectHeight),           //     draw the whole thing)
+                Color.White,                    // - The color
+                0,                              // - Rotation (none currently)
+                Vector2.Zero,                   // - Origin inside the image (top left)
+                1.0f,                           // - Scale (100% - no change)
+                flipSprite,                     // - Can be used to flip the image
+                0);                             // - Layer depth (unused)
+        }
+
+        // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+        // PRACTICE EXERCISE: Add a method to support drawing Mario crouching
+        // Like the standing & walking methods above, you should only need ONE method
+        // to handle drawing Mario crouching both left and right
+        // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    }
+}
