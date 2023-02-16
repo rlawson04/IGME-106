34e0d2fd44f006617f1d335a7db8aee4dcc0a073
Reilly Lawson <rbl2991@rit.edu>
2023-02-16 11:52:23 -0500

    Initial Commit

---
+++ Homeworks/PE Sorted List/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PE_Sorted_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
