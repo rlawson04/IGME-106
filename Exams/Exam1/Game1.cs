30b156934cbdbfba711f08bb6600282e16d9a070
Reilly Lawson <rbl2991@rit.edu>
2023-02-20 21:06:01 -0500

    Finish Edit State

--- Exams/Exam1/Game1.cs
+++ Exams/Exam1/Game1.cs
@@ -1,6 +1,15 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+enum State
+{
+    Title,
+    Edit,
+    Done
+}
 
 namespace Exam1
 {
@@ -8,6 +17,13 @@ namespace Exam1
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private State currentState = State.Title;
+        private SpriteFont defaultFont;
+        private Texture2D texture;
+        private int ducks;
+        private KeyboardState prevKBState;
+        private MouseState mState = Mouse.GetState();
+        
 
         public Game1()
         {
@@ -26,8 +42,9 @@ namespace Exam1
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            defaultFont = Content.Load<SpriteFont>("defaultFont");
+            texture = Content.Load<Texture2D>("ducky");
+           
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,16 +52,71 @@ namespace Exam1
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            KeyboardState kbstate = Keyboard.GetState();
+            
+
+            switch (currentState)
+            {
+                case State.Title:
+                    if (kbstate.IsKeyDown(Keys.Space) && prevKBState.IsKeyDown(Keys.Space))
+                    {
+                        currentState = State.Edit;
+                    }
+                    break;
+
+                case State.Edit:
+                    if (kbstate.IsKeyDown(Keys.Space) && prevKBState.IsKeyUp(Keys.Space))
+                    {
+                        currentState = State.Done;
+                    }
+                    
+                    break;
+
+                case State.Done:
+                    if (kbstate.IsKeyDown(Keys.Space) && prevKBState.IsKeyUp(Keys.Space))
+                    {
+                        currentState = State.Title;
+                    }
+                    break;
+            }
 
             base.Update(gameTime);
+
+            prevKBState = kbstate;
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Aqua);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            switch (currentState)
+            {
+                case State.Title:
+                    _spriteBatch.DrawString(defaultFont, "Reilly Lawson - Section 1", new Vector2(10,10), Color.Black);
+                    break;
+
+                case State.Edit:
+                    _spriteBatch.DrawString(defaultFont, $"{ducks}", new Vector2(10, 10), Color.Black);
+
+                    while (ducks < 6)
+                    {
+                        if (mState.LeftButton == ButtonState.Pressed)
+                        {
+                            _spriteBatch.Draw(texture, new Vector2(mState.X, mState.Y), Color.White);
+                            ducks++;
+                        }
+                    }
+
+                    break;
+
+                case State.Done:
+
+                    break;
+            }
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
