5d066d504e7311dbb8ddc83edabcd2d7d751b7aa
Reilly Lawson <rbl2991@rit.edu>
2023-02-26 15:35:42 -0500

    Final version with comments

--- PEs/DictionaryVsList_Starter/Program.cs
+++ PEs/DictionaryVsList_Starter/Program.cs
@@ -18,22 +18,30 @@ namespace DictionaryVsList_Starter
 			List<String> wordList = reader.WordList;
 			Dictionary<String, bool> wordDictionary = reader.WordDictionary;
 
-			// *********************
-			// Put your code between here...
+			//**************************************************************************
 
 			string userInput = " ";
 			string doubleWord = " ";
-			bool isDouble = false;
+			string userWord = " ";
+			System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
 
+			// Main loop that asks user for input until Q is given
 			while (userInput != "Q")
 			{
-				
+				// Main menu prompt
                 Console.Write("Do you want to search the LIST (L), DICTIONARY (D), or QUIT (Q)?\n> ");
                 userInput = Console.ReadLine().ToUpper();
 
+				// Switch to handle user input
                 switch (userInput)
                 {
+					// Uses a list to search through the list of words for any words
+					// made up of two smaller repeated words
                     case "L":
+                        watch.Reset();
+                        watch.Start();
+
+						// Searching through the list
                         for(int i = 0; i < wordList.Count; i++)
 						{
 							doubleWord = wordList[i] + wordList[i];
@@ -44,17 +52,70 @@ namespace DictionaryVsList_Starter
 							}
 							
 						}
+						watch.Stop();
+
+						// Reporting how long it took
+						Console.WriteLine("List search took " + watch.Elapsed.TotalSeconds
+							+ "seconds");
                         Console.WriteLine();
                         break;
 
+                    // Uses a dictionary to search through the list of words for any words
+                    // made up of two smaller repeated words and then asks user for a word
+					// which is then searched through the list and returns if it is or isn't a double
                     case "D":
-						Console.WriteLine("CHECKING DICTIONARY");
+						watch.Reset();
+						watch.Start();
+
+						// Searching through the dictionary
+						for(int i = 0; i<wordList.Count; i++)
+						{
+                            doubleWord = wordList[i] + wordList[i];
+							if(wordDictionary.ContainsKey(doubleWord))
+							{
+                                Console.WriteLine(doubleWord);
+								wordDictionary[doubleWord] = true;
+                            }
+                            
+						}
+						watch.Stop();
+
+						// Reporting how long it took
+                        Console.WriteLine("Dictionary search took " + watch.Elapsed.TotalSeconds
+                            + "seconds");
                         Console.WriteLine();
+
+						// Searching using user input
+						while (userWord != "quit")
+						{
+                            Console.Write("Which word would you like to check?\n>");
+                            userWord = Console.ReadLine();
+
+							if (wordDictionary.ContainsKey(userWord))
+							{
+								if (wordDictionary[userWord] == true)
+								{
+									Console.WriteLine("The word IS a double!\n");
+								}
+								else
+								{
+									Console.WriteLine("The word IS NOT a double\n");
+								}
+							}
+							else
+							{
+								Console.WriteLine("The word is not in the list\n");
+							}
+                        }
+
+						Console.WriteLine();
                         break;
 
+					// Quits from the loop
 					case "Q":
 						break;
 
+					// In case user enters something besides the prompted values
                     default:
                         Console.WriteLine("Sorry, could you enter either the letter 'D', 'L', or 'Q'?");
                         Console.Write("Do you want to search the LIST (L) or DICTIONARY (D)?\n> ");
@@ -64,12 +125,8 @@ namespace DictionaryVsList_Starter
             }
 
 			Console.WriteLine("Thank you for searching!");
-			
 
-
-			// ...and here.
-			// *********************
-
+			// *************************************************************************
 		}
 	}
 }
