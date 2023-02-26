7e2ae1d8a8d0a5e9eba93ae85996d76b0bf1a390
Reilly Lawson <rbl2991@rit.edu>
2023-02-26 14:16:07 -0500

    Added starter code to git

---
+++ PEs/DictionaryVsList_Starter/PracticeExerciseFileReader.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace DictionaryVsList_Starter
+{
+    // DO NOT MODIFY ANYTHING EXCEPT THE BLOCK MARKED
+    // WITH **** and "REPLACE THIS"
+    class PracticeExerciseFileReader
+	{
+		// Fields to hold data and stream reader
+		private List<String> wordList;
+		private Dictionary<String, bool> wordDictionary;
+
+		/// <summary>
+		/// Gets the list of words that have been read in
+		/// </summary>
+		public List<string> WordList { get { return wordList; } }
+
+		/// <summary>
+		/// Gets the dictionary of words that have been read in
+		/// </summary>
+		public Dictionary<String, bool> WordDictionary { get { return wordDictionary; } }
+
+		/// <summary>
+		/// Loads words from an external file
+		/// </summary>
+		public PracticeExerciseFileReader()
+		{
+            // Create the data structures
+            wordList = new List<string>();
+            wordDictionary = new Dictionary<string, bool>();
+
+            // Initialize the StreamReader
+            StreamReader reader = null;
+
+            try
+            {
+                // Open the file and read it into both the list and dictionary
+                reader = new StreamReader("..\\..\\words.txt");
+
+                // Loop through the file, one line at a time
+                string line = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // ***************************************************************************
+                    // *** REPLACE THIS with code to add the word to both the list and the dictionary
+                    Console.WriteLine(line);
+                    // ***************************************************************************
+                }
+            }
+            catch (Exception e)
+			{
+				Console.WriteLine("Error loading word list: " + e.Message + "\n");
+				Console.WriteLine("If you ran this program with Ctrl+F5 (or 'Start without Debugging'), try running it with F5 instead (or vice versa)");
+			}
+            finally
+            {
+                // Ensure that we can close the file, as long
+                // as it was actually opened in the first place
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+        }
+	}
+}
---
+++ PEs/DictionaryVsList_Starter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryVsList_Starter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Creates a new file reader, which loads a file of words
+			// into both a list and a dictionary
+			PracticeExerciseFileReader reader = new PracticeExerciseFileReader();
+
+			// Get the two data structures needed for the exercise
+			List<String> wordList = reader.WordList;
+			Dictionary<String, bool> wordDictionary = reader.WordDictionary;
+
+			// *********************
+			// Put your code between here...
+
+
+
+
+
+			// ...and here.
+			// *********************
+
+		}
+	}
+}
