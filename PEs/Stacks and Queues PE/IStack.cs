2b8a020b1a69ffda9e7ca1e7bfd64499324fbe2f
rlawson04 <rbl2991@rit.edu>
2023-03-08 11:26:15 -0500

    Implement IStack into MyStack

---
+++ PEs/Stacks and Queues PE/IStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_and_Queues_PE
+{
+        // DO NOT MODIFY THIS CODE
+        interface IStack<T>
+        {
+            /// <summary>
+            /// Gets the current count of items in the stack
+            /// </summary>
+            int Count { get; }
+            /// <summary>
+            /// Gets whether or not there are items in the stack
+            /// </summary>
+            bool IsEmpty { get; }
+            /// <summary>
+            /// Look at and return the top of the stack. Returns
+            /// null if the stack is empty.
+            /// </summary>
+            T Peek();
+            /// <summary>
+            /// Adds new data to the top of the stack.
+            /// </summary>
+            /// <param name="item">The data to add</param>
+            void Push(T item);
+            /// <summary>
+            /// Removes and returns the data on top
+            /// of the stack. Returns null if the stack is empty.
+            /// </summary>
+            T Pop();
+        }
+}
