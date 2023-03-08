31585e6ccd48597969a3d6129133e13b1e901a90
rlawson04 <rbl2991@rit.edu>
2023-03-08 10:24:55 -0500

    Initial Commit

---
+++ PEs/Stacks and Queues PE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Stacks_and_Queues_PE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
