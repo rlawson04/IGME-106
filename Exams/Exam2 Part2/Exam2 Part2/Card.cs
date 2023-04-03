9d508ea49f8c12f692210b3ace85fe522f8d77ca
Reilly Lawson <rbl2991@rit.edu>
2023-04-03 16:31:09 -0400

    Exam 2 Initial Commit

---
+++ Exams/Exam2 Part2/Exam2 Part2/Card.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam2_Part2
+{
+    /*----------------------------------------------------------------
+      * This class represents a single playing card from a 52 card deck
+      * 
+      * suit: a value from 0 - 3
+      * 0 = diamonds, 1 = clubs, 2 = hearts, 3 = spades
+      * 
+      * value: a value from 1 - 13
+      * 1 = ace, 2 - 10  = face value, 11 = jack, 12 = queen, 13 = king
+      * 
+      * DO NOT ALTER ANY CODE IN THIS CLASS!
+      * Prof. Bierre
+      -----------------------------------------------------------------*/
+    internal class Card
+    {
+        // fields
+        private int suit; // suit of the card (0 - 3)
+        private int value; // value of the card (1 - 13)
+        private string[] suits = { "Diamonds", "Clubs", "Hearts", "Spades" };
+
+        // constructor
+        public Card(int suit, int value)
+        {
+            this.suit = suit;
+            this.value = value;
+        }
+
+        // ToString override
+        public override string ToString()
+        {
+            string text = "";
+            switch (value)
+            {
+                case 1:
+                    text += "Ace of " + suits[suit];
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                    text += value + " of " + suits[suit];
+                    break;
+                case 11:
+                    text += "Jack of " + suits[suit];
+                    break;
+                case 12:
+                    text += "Queen of " + suits[suit];
+                    break;
+                case 13:
+                    text += "King of " + suits[suit];
+                    break;
+            }
+            return text;
+        }
+    }
+}
---
+++ Exams/Exam2 Part2/Exam2 Part2/IStack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam2_Part2
+{
+    // interface for a stack
+    internal interface IStack<T>
+    {
+        // check to see if there is data in the stack (false) or
+        // if it's empty (true).
+        bool isEmpty();
+
+        // push an item onto the stack
+        void Push(T item);
+
+        // pop the top off of the stack
+        T Pop();
+
+        // clear the stack by removing all items
+        // from the underlying data structure
+        void Clear();
+    }
+}
