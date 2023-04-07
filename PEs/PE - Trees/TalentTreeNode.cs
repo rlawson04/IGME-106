ac0a1c3c77d8d326e7b0c7487ae417103ca5b9fc
rlawson04 <rbl2991@rit.edu>
2023-04-07 11:03:57 -0400

    Finish Talent Tree Node

---
+++ PEs/PE - Trees/TalentTreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE___Trees
+{
+    internal class TalentTreeNode
+    {
+
+        string abilityName;
+        bool isLearned;
+
+        /// <summary>
+        /// Parameterized constructor that takes the name of the ability, and whether it has been learned
+        /// </summary>
+        /// <param name="abilityName"></param>
+        /// <param name="isLearned"></param>
+        public TalentTreeNode(string abilityName, bool isLearned)
+        {
+            this.abilityName = abilityName;
+            this.isLearned = isLearned;
+        }
+    }
+}
