be55868894b0c824788cdc4d3cabf3e7160d9ec8
Reilly Lawson <rbl2991@rit.edu>
2023-05-08 13:46:57 -0400

    Iniatial Commit with Graph class created

---
+++ Exams/Exam 3/Graph.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_3
+{
+    internal class Graph
+    {
+        // Fields
+
+        // Properties
+
+        // Constructors
+
+        // Methods
+    }
+}
---
+++ Exams/Exam 3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exam_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
